21da4fc9ad4746f870bc5024943cf71d289613fa
jansigurana <jansigurana@gmail.com>
2024-05-17 11:53:19 +0000

    first commit

---
+++ src/components/Tasks/styledComponents.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+export const TagsCategory = styled.p`
+  font-family:'Roboto';
+  background-color: #f3aa4e;
+  border: 0px;
+  border-radius: 20px;
+  height: 12px;
+  padding: 10px;
+  margin: 5px;
+  color: #131213;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+  `
+export const ListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 20px;
+  margin: 5px;
+  height:15px;
+  width:100%;
+  list-style-type: none;
+  background-color:  #1a171d;
+`
+export const TaskName = styled.p`
+  font-family:'Roboto';
+  font-size: 18px;
+  font-weight: normal;
+  color: #f1f5f9;
+`
